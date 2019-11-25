86a4349bf998bf67bb2c2736f948f4accfea65bc
qimiaoguo <qimiaoguo@gmail.com>
2019-11-25 15:33:14 +0800

    Updates

---
+++ precache-manifest.495885dba22f869b2e31110eb294ca80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f53c2cdc04c9e8c86f6160d901f3cd7",
+    "url": "/scoop/index.html"
+  },
+  {
+    "revision": "49a0b710ab4ecbd062cb",
+    "url": "/scoop/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a76fd62825b404a0c93a",
+    "url": "/scoop/static/js/2.ef8ba17e.chunk.js"
+  },
+  {
+    "revision": "49a0b710ab4ecbd062cb",
+    "url": "/scoop/static/js/main.d6f55bb1.chunk.js"
+  },
+  {
+    "revision": "3fb58faa170cbcfb79cd",
+    "url": "/scoop/static/js/runtime-main.530134e5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/scoop/static/media/logo.25bf045c.svg"
+  }
+]);
